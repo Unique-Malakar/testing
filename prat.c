a94c011f5bfa74cceb21fd025b9ae1845cd9e554
Uniquemalakar <linkinshadows2@gmail.com>
2021-11-13 21:21:08 -0500

    this is a message for the first box in github
    This for the second

---
+++ prat.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+
+
+typedef struct node {
+    struct node * next;
+    //struct node * prev;
+    int data;
+}Node;
+
+
+int main()
+{
+    //"what up bitchhh"
+    // int arr[] = {0,0,1,1,2,2,3,3};
+
+    // int *a = arr;
+    // printf("%d",*a);
+   
+    return 0;
+}
+
+
+void insert (Node **head, int value)
+{
+    // Node *curr;
+    // //Node *prev = NULL;
+    // Node *temp = NULL;
+    // if (head != NULL)
+    // {
+    //     curr = *head;
+    // }
+    // while(curr != NULL)
+    // {
+    //     if(curr->next < value)
+    //     {
+    //         curr = curr->next;
+
+    
+    //     }
+    // }
+
+}
+
+
+// int removeDuplicates(int* nums, int numsSize){
+
+
+// }
